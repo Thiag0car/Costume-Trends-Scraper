56b1f15200b6689098b01cd5f896001f64187e1c
Joao Guilherme <joaogsmachado1@gmail.com>
2023-05-30 17:48:29 -0300

    add get many urls

--- web_scrapping/scraper.py
+++ web_scrapping/scraper.py
@@ -8,15 +8,15 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 
 
-def pegar_sites():
-    with open('web_scrapping/websites.md', 'r') as file:
-        sites = file.read()
-    return sites
+def pegar_sites(arquivo):
+    with open(f'web_scrapping/{arquivo}.md', 'r') as file:
+        websites = file.read()
+    return websites
 
 
-def extrair_urls():
+def extrair_urls(arquivo):
     pattern = r'\((.*?)\)'
-    urls = re.findall(pattern, pegar_sites())
+    urls = re.findall(pattern, pegar_sites(arquivo))
     return urls
 
 
@@ -42,8 +42,36 @@ def teste_um_site(url):
     print(texto)
 
 
+def extract_links_from_pages():
+    urls = extrair_urls('blogs')
+    todos_links = []
+    for url in urls:
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        links = [
+            link.get('href')
+            for link in soup.find_all('a')
+            if link and link.get('href', '').startswith('https://')
+            and not link.get('href', '').endswith('.pdf')
+            and 'facebook' not in link.get('href', '')
+            and 'instagram' not in link.get('href', '')
+            and 'youtube' not in link.get('href', '')
+            and 'twitter' not in link.get('href', '')
+            and 'pinterest' not in link.get('href', '')
+            and 'google' not in link.get('href', '')
+            and 'linkedin' not in link.get('href', '')
+            and 'itunes' not in link.get('href', '')
+            and 'tiktok' not in link.get('href', '')
+        ]
+        todos_links.extend(links)
+    todos_links = list(set(todos_links)) # Remove duplicados
+    return todos_links
+
+
 def scrape_websites():
-    urls = extrair_urls()
+    urls = extrair_urls('artigos')
+    links = extract_links_from_pages()
+    urls.extend(links)
     arquivo_path = 'web_scrapping/dados_sites.csv'
 
     if os.path.exists(arquivo_path):
